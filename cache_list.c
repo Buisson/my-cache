a1379d504ed14a81a78b21175b53c596306c6593
Alexandre CAZALA <alexandre.cazala@gmail.com>
2015-06-10 16:03:52 +0200

    attentoin c'est faux

--- cache_list.c
+++ cache_list.c
@@ -4,12 +4,9 @@
 #include <stdbool.h>
 
 struct Cache_List *Cache_List_Create(){
-	printf("iciCache_List_Create\n");
-	struct Cache_List* ptrLst = (struct Cache_List*) malloc(sizeof(struct Cache_List));
-	
-	struct Cache_Block_Header *header = NULL;
-	
-	ptrLst->pheader=header;
+	printf("Cache_List_Create\n");
+	struct Cache_List* ptrLst = malloc(sizeof(struct Cache_List));
+	ptrLst->pheader=NULL;
 	ptrLst->next=NULL;
 	ptrLst->prev=NULL;
 
@@ -29,37 +26,52 @@ void Cache_List_Delete(struct Cache_List *list){
 
 void Cache_List_Append(struct Cache_List *list, struct Cache_Block_Header *pbh){
 	printf("iciAppend\n");	
-	struct Cache_List* new = (struct Cache_List*)malloc(sizeof(struct Cache_List));
-	
-	new->next = NULL;
-	new->pheader = pbh;
-
-	struct Cache_List *temp = list;
-	while(temp->next!=NULL){
-		temp = temp->next;
+	if(Cache_List_Is_Empty(list)){
+		list->pheader=pbh;
+	} else {
+		struct Cache_List* new = Cache_List_Create();
+		struct Cache_List* tmp = list;
+		// on parcourt la liste pour récupérer le dernier
+		while ((tmp->next) != NULL) {
+			tmp = tmp->next;
+		} 
+		new->prev = tmp;
+		new->pheader = pbh;
+		tmp->next = new;
+		printf("FINAppend\n");
 	}
-	temp->next = new;	
-	new->prev = temp;
-	printf("FINAppend\n");
+	
 }
 
 void Cache_List_Prepend(struct Cache_List *list, struct Cache_Block_Header *pbh){
 	printf("iciPrepend\n");
-	struct Cache_List* tmp = list->next;
-	struct Cache_List* new = (struct Cache_List*)malloc(sizeof(struct Cache_List));
-
-	new->next = tmp;
-	new->prev = list;
-	new->pheader = pbh;
-	if(tmp->prev!=NULL)
-		tmp->prev=new;
+	if(Cache_List_Is_Empty(list)){
+		list->pheader=pbh;
+	} else {	
+		struct Cache_List* new = Cache_List_Create();
+		new->pheader = pbh;
+		if (Cache_List_Is_Empty(list)) {
+			list = new;
+		} else {
+			struct Cache_List* tmp = Cache_List_Create();
+			while (tmp->prev != NULL) tmp = tmp->prev;
+			new->next = tmp;
+			tmp->prev = new;
+		}
+	}
 }
 
 
 struct Cache_Block_Header *Cache_List_Remove_First(struct Cache_List *list){
 	printf("iciCache_List_Remove_First\n");	
 	if(!Cache_List_Is_Empty(list)){	
-		return Cache_List_Remove(list,list->next->pheader);
+		struct Cache_List* tmp = list;
+		while (tmp->prev != NULL) tmp = tmp->prev;
+		struct Cache_Block_Header *bheader = tmp->pheader;	
+		tmp = tmp->next;
+		Cache_List_Delete(tmp->prev);
+		tmp->prev =NULL;
+		return bheader;
 	}
 	return NULL;
 }
@@ -68,31 +80,56 @@ struct Cache_Block_Header *Cache_List_Remove_First(struct Cache_List *list){
 struct Cache_Block_Header *Cache_List_Remove_Last(struct Cache_List *list){
 	printf("iciCache_List_Remove_Last\n");
 	if(!Cache_List_Is_Empty(list)){	
-		struct Cache_List *temp = list;
-		while(temp->next!=NULL){
-			temp = temp->next;
+		struct Cache_List *tmp = list;
+		while(tmp->next!=NULL){
+			tmp = tmp->next;
 		}
-		return Cache_List_Remove(list,temp->pheader);
+		return Cache_List_Remove(list,tmp->pheader);
 	}
 	return NULL;
 }
 
 struct Cache_Block_Header *Cache_List_Remove(struct Cache_List *list,struct Cache_Block_Header *pbh){
-	printf("iciCache_List_Remove\n");	
-	struct Cache_List * tmp = list;
-	while((list->next) != NULL){
-		if((tmp->pheader)==pbh){
-			tmp->prev->next = tmp->next;
-			if(tmp->next!=NULL)
-				tmp->next->prev = tmp->prev;
-
-			struct Cache_Block_Header *retour = tmp->pheader;			
-			free(tmp);
-			printf("##########################################%p############################",retour);
-			return retour;
-		}
-		tmp = tmp->next;
+	printf("Cache_List_Remove\n");	
+	struct Cache_List* tmp;
+	if (Cache_List_Is_Empty(list)) {
+		printf("AHAHAHAHAHAH t'es con alex\n");
 	}
+	for (tmp = list; tmp->pheader != pbh && tmp->next != NULL;tmp = tmp->next) {}
+	if (pbh == tmp->pheader) {
+		printf("REMOVE : Trouvé 1\n");
+		struct Cache_Block_Header* htmp = tmp->pheader;
+		printf("%p\n",htmp);
+		printf("%p\n",pbh);
+		struct Cache_List* tmp_prev = tmp->prev;
+		struct Cache_List* tmp_next = tmp->next;
+		printf("ici ?\n");
+		printf("%p\n",tmp);
+		tmp_prev->next = tmp->next;
+		tmp_next->prev = tmp->prev;
+		printf("hey ?\n");
+		/*if(tmp->next!=NULL) {
+			tmp2 = tmp->next;
+			tmp2->prev = tmp->prev;
+		}*/
+		struct Cache_Block_Header *retour = tmp->pheader;			
+		free(tmp);
+		printf("##########################################%p############################",retour);
+		return retour;
+	}
+	// au cas où on part dans l'autre sens
+	for (tmp = list; tmp->pheader != pbh && tmp->prev != NULL;tmp = tmp->prev) {}
+	printf("REMOVE : deuxième for \n");
+	if((tmp->pheader)==pbh){
+		(tmp->next)->prev = tmp->prev;
+		if(tmp->prev!=NULL)
+			(tmp->prev)->next = tmp->prev;
+		struct Cache_Block_Header *retour = tmp->pheader;			
+		free(tmp);
+		printf("Boucle de prev %p",retour);
+		return retour;
+	}
+	return NULL;
 }
 
 
@@ -104,7 +141,7 @@ void Cache_List_Clear(struct Cache_List *list){
 
 bool Cache_List_Is_Empty(struct Cache_List *list){
 	printf("iciCache_List_Is_Empty\n");	
-	return (list->next==NULL && list->prev==NULL) ? true : false;
+	return (list->pheader==NULL && list->next==NULL && list->prev==NULL) ? true : false;
 }
 
 void Cache_List_Move_To_End(struct Cache_List *list,struct Cache_Block_Header *pbh){	
