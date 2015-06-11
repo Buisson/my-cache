43d45977088602c5070b867684c932521c70d8b5
Alexandre CAZALA <alexandre.cazala@gmail.com>
2015-06-11 16:09:17 +0200

    Voila version presque finie, légère optimisation à prévoir

--- cache_list.c
+++ cache_list.c
@@ -1,9 +1,8 @@
 /*!
  * \file low_cache.c
  * 
- * \author Thibaut SORIANO
- * \author Aina Rapenomanana
- * \author Pascal Tung
+ * \author Aurelien Colombet
+ * \author Alexandre Cazala
  */
 
 #include <stdlib.h>
@@ -44,9 +43,11 @@ void Cache_List_Append(struct Cache_List *list, struct Cache_Block_Header *pbh)
     struct Cache_List *new_elem = Cache_List_Create();
     new_elem->pheader = pbh;
 
-    struct Cache_List *last = list->next;
-    for (; last != list; last = last->next);
-
+    struct Cache_List *last;
+    // on va à la fin de la liste
+    for (last = list->next ; last != list; last = last->next);
+		
+	// on insère notre élément
     new_elem->prev   = last->prev;
     last->prev->next = new_elem;
     last->prev       = new_elem;
@@ -58,25 +59,29 @@ void Cache_List_Prepend(struct Cache_List *list, struct Cache_Block_Header *pbh)
 {
     struct Cache_List *new_elem = Cache_List_Create();
     new_elem->pheader = pbh;
-
-    struct Cache_List *second = list->next;
-
-    new_elem->prev     = second->prev;
-    second->prev->next = new_elem;
-    second->prev       = new_elem;
-    new_elem->next     = second;
+	struct Cache_List *first;
+	// Par sécurité on va au début de la liste
+	for (first = list->prev ; first != list; first = first->prev);
+    new_elem->prev = first->next->prev;
+    first->next = new_elem;
+    first->next->prev = new_elem;
+    new_elem->next = first->next;
 }
 
 /*! Retrait du premier élément */
 struct Cache_Block_Header *Cache_List_Remove_First(struct Cache_List *list)
 {
-    struct Cache_List* first = list->next;
+	struct Cache_List *tmp;
+	// Par sécurité on va au début de la liste
+	for (tmp = list->prev ; tmp != list; tmp = tmp->prev);
+	// on récup le deuxième
+	tmp = tmp->next;
                     
-    first->prev->next = first->next;
-    first->next->prev = first->prev;
+    tmp->prev->next = tmp->next;
+    tmp->next->prev = tmp->prev;
 
-    struct Cache_Block_Header *header = first->pheader;
-    free(first);
+    struct Cache_Block_Header *header = tmp->pheader;
+    free(tmp);
     return header;
 }
 
@@ -84,6 +89,7 @@ struct Cache_Block_Header *Cache_List_Remove_First(struct Cache_List *list)
 struct Cache_Block_Header *Cache_List_Remove_Last(struct Cache_List *list)
 {
     struct Cache_List* last;
+    // on va au dernier de la liste
     for (last = list->next; last != list; last = last->next);
 
     last->prev->next = last->next;
@@ -123,12 +129,14 @@ void Cache_List_Clear(struct Cache_List *list){
 
 /*! Test de liste vide */
 bool Cache_List_Is_Empty(struct Cache_List *list){
-    return list->next == list->prev;
+	// si jamais on ne peut pas partir de l'élément et qu'on a pas d'header
+    return (list->next == list->prev) && (list->pheader == NULL);
 }
 
 /*! Transférer un élément à la fin */
 void Cache_List_Move_To_End(struct Cache_List *list, struct Cache_Block_Header *pbh)
 {
+	// on l'enlève et on le met a la fin
     Cache_List_Remove(list, pbh);
     Cache_List_Append(list, pbh);
 }
@@ -136,6 +144,7 @@ void Cache_List_Move_To_End(struct Cache_List *list, struct Cache_Block_Header *
 /*! Transférer un élément  au début */
 void Cache_List_Move_To_Begin(struct Cache_List *list, struct Cache_Block_Header *pbh)
 {
+	// on l'enlève et on le met au début
     Cache_List_Remove(list, pbh);
     Cache_List_Prepend(list, pbh);
 }
